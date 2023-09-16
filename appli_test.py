a458ae73646911b3bd7776aabce853532519d721
aymankhezami <96355806+aymankhezami@users.noreply.github.com>
2023-09-16 11:31:32 +0200

    optimize histograms and adapt the size table

--- appli_test.py
+++ appli_test.py
@@ -14,12 +14,17 @@ colors = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99',
           '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6', '#6a3d9a']
 
 
+
 fig1 = px.histogram(
     df,
     x="brand",
     color="series",
-    title="Distribution of Brands by Processor Generation"
+    title="Distribution of Brands by Processor Generation",
+    color_discrete_sequence=colors,
+    height=600
 )
+
+
 fig1.update_xaxes(title="Brand")
 fig1.update_yaxes(title="Frequency")
 
@@ -27,12 +32,25 @@ fig2 = px.histogram(
     df,
     x="nGeneration2",
     color="brand",
-    labels={"brand": "brand of intel microchip"},
-    title="Distribution of Brands by Processor Generation"
+    title="Distribution of Brands by Processor Generation",
+    color_discrete_sequence=colors,
+    height=600
 )
-
 fig2.update_xaxes(title="Generation")
 fig2.update_yaxes(title="Frequency")
+fig2.update_traces(marker=dict(line=dict(color='white', width=3)))
+
+fig3 = px.scatter(
+    df,
+    x="nGeneration2",
+    y="nbr_cores",
+    color="series",
+    color_discrete_sequence=colors,
+    title="Number of cores per generation for each series"
+)
+
+fig3.update_xaxes(title="Generation")
+fig3.update_yaxes(title="Number of cores")
 
 
 external_stylesheets = [dbc.themes.CERULEAN]
@@ -53,15 +71,15 @@ app.layout = dbc.Container([
                 ],
                 id="drop_down_gen"
             ),
-        ], width=4),
+        ], width=2),
 
         dbc.Col([
             html.Label('Brand : '),
             dcc.Dropdown(
-                options=intel["brand"].unique(),
+                options= intel["brand"].unique(),
                 id="drop_down_brand"
             ),
-        ], width=3),
+        ], width=2),
 
         dbc.Col([
             html.Label('Number of cores : '),
@@ -78,22 +96,25 @@ app.layout = dbc.Container([
     html.Hr(),
 
     dbc.Row([
-        dbc.Col([dash_table.DataTable(data=intel.to_dict('records'), page_size=12,
-                                      id="table",
+        dbc.Col([dash_table.DataTable(data=intel.to_dict('records'), page_size=10,
+                                      id='table',
                                       style_table={'overflowX': 'auto'},
                                       style_cell={
             # all three widths are needed
-            'minWidth': '100px', 'width': '100%', 'maxWidth': '600px',
+            'minWidth': '50px', 'width': '100%', 'maxWidth': '600px',
+            'height':'30px',
             'overflow': 'hidden',
             'textOverflow': 'ellipsis',
-        })], width=8),
-    ]),
+        })], width=12),
+    ],style={'height':'100%','minHeight':'200px'}),
     html.Hr(),
     dbc.Row([
-        dbc.Col([dcc.Graph(id='histogram_B', figure=fig1)], width=6),
-        dbc.Col([dcc.Graph(id='histogram_G', figure=fig2)], width=5)
+        dbc.Col([dcc.Graph(id='histogram_B', figure=fig1)], width=4),
+        dbc.Col([dcc.Graph(id='histogram_G', figure=fig2)], width=4),
+        dbc.Col([dcc.Graph(id='scatter_A', figure=fig3)], width=4),
 
     ]),
+
 
 
 ], fluid=True)
